d613429923a9a2b1c6ecf9e5cf35b9cdb94c7094
Jaiden Chicote <jedezzio@gmail.com>
2020-05-25 12:56:21 +1000

    Improved Post Precessing + Basic Tiles + Animation scene + Lightmapped scene assets (not prefab) + Scene Construction

--- Assets/Scripts/Test/Test_Movement.cs
+++ Assets/Scripts/Test/Test_Movement.cs
@@ -17,13 +17,15 @@ public class Test_Movement : MonoBehaviour
         if (Input.GetButton("Horizontal"))
         {
             float movementX = baseSpeed * Input.GetAxis("Horizontal");
-            transform.position += transform.right * movementX * Time.fixedDeltaTime;
+            //transform.position += transform.right * movementX * Time.fixedDeltaTime;
+            float yaw = movementX * 0.5f;
+            transform.Rotate(0, yaw, 0);
         }
 
         if (Input.GetButton("Vertical"))
         {
             float movementY = baseSpeed * Input.GetAxis("Vertical");
-            transform.position += transform.forward * movementY * Time.fixedDeltaTime;
+            transform.position += transform.forward * movementY * 0.1f;
         }
 
     }
