8fb5f0b8c4438c122313b8f00eb96bf7c8974d36
CameronBossalini <cjb170530@utdallas.edu>
2020-06-12 23:23:03 -0500

    TextMeshPro install, Timeline tweaks

---
+++ Assets/Scripts/Lighting/LampControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Light[] lights;
+    void Start() {
+        lights = GetComponentsInChildren<Light>();
+    }
+
+    public void DoFlicker() {
+        StartCoroutine(DoFlickerCoroutine());
+    }
+
+    IEnumerator DoFlickerCoroutine() {
+        SwitchOff();
+        yield return new WaitForSeconds(0.2f);
+        SwitchOn();
+        yield return new WaitForSeconds(0.3f);
+        for (int i = 0; i < 5; i++) {
+            SwitchOff();
+            yield return new WaitForSeconds(0.1f);
+            SwitchOn();
+        }
+        SwitchOff();
+        yield return new WaitForSeconds(2);
+        SwitchOn();
+    }
+
+    public void SwitchOff() {
+        foreach (Light l in lights) {
+            l.enabled = false;
+        }
+    }
+
+    public void SwitchOn() {
+        foreach (Light l in lights) {
+            l.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Quicktime/QuickTimeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using System;
+using TMPro;
+
+public class QuickTimeController : MonoBehaviour
+{
+    [Serializable]
+    public struct TimeEvent {
+        public string name;
+        public KeyCode key;
+        public PlayableDirector failureTimeLine;
+        public bool completed;
+    }
+
+    public PlayableDirector mainTimeline;
+    public TextMeshProUGUI keyView;
+    public TimeEvent[] events;
+    private TimeEvent ActiveEvent;
+
+    private TimeEvent getEvent(string key) {
+        foreach (TimeEvent t in events) {
+            if (t.name.Equals(key)) {
+                return t;
+            }
+        }
+        throw new System.ArgumentException("Event with name: " + key + " does not exist.");
+    }
+
+
+    public void ActivateEvent(string key) {
+        ActiveEvent = getEvent(key);
+        StartCoroutine(performEvent());
+    }
+
+    private IEnumerator performEvent() {
+        keyView.text = ActiveEvent.key.ToString();
+        keyView.enabled = true;
+        yield return new WaitForSeconds(5);
+        keyView.enabled = false;
+        if (!ActiveEvent.completed) {
+            ActiveEvent.failureTimeLine.time = mainTimeline.time;
+            mainTimeline.Stop();
+            ActiveEvent.failureTimeLine.Play();
+        }
+    }
+
+    public void DoLampFlicker() {
+        GameObject[] lamps = GameObject.FindGameObjectsWithTag("Lamp");
+        foreach (GameObject lamp in lamps) {
+            lamp.GetComponent<LampControl>().DoFlicker();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        keyView.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(ActiveEvent.key)) {
+            ActiveEvent.completed = true;
+            keyView.enabled = false;
+        }
+    }
+}
